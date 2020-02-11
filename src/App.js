b9235d0d1469f5f4fc60a44a3eed28b0943fe346
hcampbell3 <59670820+hcampbell3@users.noreply.github.com>
2020-02-10 20:43:18 -0500

    Navigation (#17)
    * init the project3
    
    * init the project4
    
    * init the project4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Routes from "./Routes";
 
 
 export default class App extends Component {
   render() {
-    return <div>Fisher Bookstore</div>
+    return (
+      <div className="App container">
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand>
+              <Link to="/">Fisher Bookstore</Link>
+            </Navbar.Brand>
+        </Navbar>
+        <Routes />
+      </div>
+    );
   }
 };
