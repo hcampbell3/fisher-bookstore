34ac88fed7a14a422a47cb507e8c70604ba99469
Haylee Campbell <59670820+hcampbell3@users.noreply.github.com>
2020-02-19 21:31:30 -0500

    push authors-1

--- src/authors/Authors.js
+++ src/authors/Authors.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Authors.css";
+import { AuthorDisplay } from "./AuthorDisplay";
 
-export default function Authors() {
+
+export default class Authors extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            authors: [
+                {
+                    id: 1, 
+                    title: "Domain Driven Design",
+                    author: "Eric Evans",
+                    isbn: "978-0321125217"
+                },
+                {
+                    id:2,
+                    title: "Accelerate",
+                    author: "Nicole Forsgren",
+                    isbn: "978-1942788331"
+                }
+            ]
+        };
+    }
+    
+    render() {
+        return (
+            <div className="Authors">
+                <div className="lander">
+                    <AuthorDisplay authors={this.state.authors} />
+                </div>
+            </div>
+        );
+    }    
+    }
+/* export default function Authors() {
     return (
         <div className="Authors">
             <div className="lander">
@@ -10,4 +43,4 @@ export default function Authors() {
             </div>
         </div>
     );
-}
+} */
