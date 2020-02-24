4fea59855307565744c4dd4d1cf9605485b4f084
Haylee Campbell <59670820+hcampbell3@users.noreply.github.com>
2020-02-24 18:58:34 -0500

    authors list

--- src/authors/Authors.js
+++ src/authors/Authors.js
@@ -19,6 +19,42 @@ export default class Authors extends Component {
                     title: "Accelerate",
                     author: "Nicole Forsgren",
                     isbn: "978-1942788331"
+                },
+                {
+                    id: 3, 
+                    title: "Haylee Learns how to Spell",
+                    author: "Haylee Cmapbell",
+                    isbn: "978-0321125217"
+                },
+                {
+                    id: 4, 
+                    title: "Amber Makes Scallops",
+                    author: "Amber Rudy",
+                    isbn: "978-0321125217"
+                },
+                {
+                    id: 5, 
+                    title: "David does Karate: a Failure",
+                    author: "Dr. Do-Little",
+                    isbn: "978-0321125217"
+                },
+                {
+                    id: 6, 
+                    title: "Someone wants an Apology",
+                    author: "No Huay",
+                    isbn: "978-0321125217"
+                },
+                {
+                    id: 7, 
+                    title: "The Life of Chad",
+                    author: "Pi the Tiger",
+                    isbn: "978-0321125217"
+                },
+                {
+                    id: 8, 
+                    title: "Marvel Returns pt.3000",
+                    author: "Steve Rogers",
+                    isbn: "978-0321125217"
                 }
             ]
         };
