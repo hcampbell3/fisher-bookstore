3f610cd2f4555da46cd1b289519851f34b45cb19
Haylee Campbell <59670820+hcampbell3@users.noreply.github.com>
2020-03-31 16:26:49 -0400

    bookstore-lab7 push

--- src/authors/Authors.js
+++ src/authors/Authors.js
@@ -1,82 +1,20 @@
-import React, { Component } from "react";
-import "./Authors.css";
-import { AuthorDisplay } from "./AuthorDisplay";
+import React, { useEffect, useState } from "react";
+import {AuthorDisplay } from "./AuthorDisplay";
 
-
-export default class Authors extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            authors: [
-                {
-                    id: 1, 
-                    title: "Domain Driven Design",
-                    author: "Eric Evans",
-                    isbn: "978-0321125217"
-                },
-                {
-                    id:2,
-                    title: "Accelerate",
-                    author: "Nicole Forsgren",
-                    isbn: "978-1942788331"
-                },
-                {
-                    id: 3, 
-                    title: "Haylee Learns how to Spell",
-                    author: "Haylee Cmapbell",
-                    isbn: "978-0321125217"
-                },
-                {
-                    id: 4, 
-                    title: "Amber Makes Scallops",
-                    author: "Amber Rudy",
-                    isbn: "978-0321125217"
-                },
-                {
-                    id: 5, 
-                    title: "David does Karate: a Failure",
-                    author: "Dr. Do-Little",
-                    isbn: "978-0321125217"
-                },
-                {
-                    id: 6, 
-                    title: "Someone wants an Apology",
-                    author: "No Huay",
-                    isbn: "978-0321125217"
-                },
-                {
-                    id: 7, 
-                    title: "The Life of Chad",
-                    author: "Pi the Tiger",
-                    isbn: "978-0321125217"
-                },
-                {
-                    id: 8, 
-                    title: "Marvel Returns pt.3000",
-                    author: "Steve Rogers",
-                    isbn: "978-0321125217"
-                }
-            ]
-        };
-    }
+export default function Authors (props) {
+    const [data, setData] = useState([]);
     
-    render() {
+    useEffect(() => {
+        fetch("https://localhost:5001/api/authors/")
+        .then(response => response.json())
+        .then(data => setData(data));
+    }, []);
+
         return (
             <div className="Authors">
                 <div className="lander">
-                    <AuthorDisplay authors={this.state.authors} />
+                    <AuthorDisplay authors={data} />
                 </div>
             </div>
         );
-    }    
-    }
-/* export default function Authors() {
-    return (
-        <div className="Authors">
-            <div className="lander">
-                <h1>Authors</h1>
-                <p>Welcome to Fisher Bookstore.</p>
-            </div>
-        </div>
-    );
-} */
+    }  
--- src/books/Books.js
+++ src/books/Books.js
@@ -1,72 +1,21 @@
-import React, { Component } from "react";
-import "./Books.css";
+import React, { useEffect, useState } from "react";
 import { BookDisplay } from "./BookDisplay";
 
-export default class Books extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            books: [
-                {
-                    id: 1, 
-                    title: "Domain Driven Design",
-                    author: "Eric Evans",
-                    isbn: "978-0321125217"
-                },
-                {
-                    id:2,
-                    title: "Accelerate",
-                    author: "Nicole Forsgren",
-                    isbn: "978-1942788331"
-                },
-                {
-                    id: 3, 
-                    title: "Haylee Learns how to Spell",
-                    author: "Haylee Cmapbell",
-                    isbn: "978-0321125217"
-                },
-                {
-                    id: 4, 
-                    title: "Amber Makes Scallops",
-                    author: "Amber Rudy",
-                    isbn: "978-0321125217"
-                },
-                {
-                    id: 5, 
-                    title: "David does Karate: a Failure",
-                    author: "Dr. Do-Little",
-                    isbn: "978-0321125217"
-                },
-                {
-                    id: 6, 
-                    title: "Someone wants an Apology",
-                    author: "No Huay",
-                    isbn: "978-0321125217"
-                },
-                {
-                    id: 7, 
-                    title: "The Life of Chad",
-                    author: "Pi the Tiger",
-                    isbn: "978-0321125217"
-                },
-                {
-                    id: 8, 
-                    title: "Marvel Returns pt.3000",
-                    author: "Steve Rogers",
-                    isbn: "978-0321125217"
-                }
-            ]
-        };
-    }
+export default function Books (props) {
+    const [data, setData] = useState([]);
     
-    render() {
+    useEffect(() => {
+        fetch("https://localhost:5001/api/books/")
+        .then(response => response.json())
+        .then(data => setData(data));
+    }, []);
+
         return (
             <div className="Books">
                 <div className="lander">
-                    <BookDisplay books={this.state.books} />
+                    <BookDisplay books={data} />
                 </div>
             </div>
         );
     }    
-    }
     
